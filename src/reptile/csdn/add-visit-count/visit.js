17697c7450d5b7db6e0355c0cda1203912b66915
benxia <416854144@qq.com>
2017-03-02 16:37:43 +0800

    reptile modified

--- src/reptile/csdn/add-visit-count/visit.js
+++ src/reptile/csdn/add-visit-count/visit.js
@@ -1,21 +1,22 @@
 const {loadUTF8} = require('../../server');
 const cheerio = require('cheerio');
 
-let target = 100;
+let target = 2000;
 const preUrl = "http://blog.csdn.net/y416854144/article/details/";
 let sites = [
-  {title: '两道Javascript-前端面试题', index: '56844013'},
-  {title: 'Activiti 自定义事件监听和邮件通知', index: '54342060'},
-  {title: 'Activiti 自定义用户管理和组管理', index: '54315929'},
-  {title: '如何生成可导入数据库的亿级别数据', index: '52185350'},
-  {title: '<MongoDB | Mysql>亿级别---数据生成及高效率导入', index: '52181366'},
-  {title: 'Express session 覆盖或重写', index: '51862761'},
-  {title: 'Angularjs之ngModel中的值验证绑定', index: '51373126'},
-
-  {title: 'jQuery 技术讨论', index: '50687942'},
-  {title: 'Ionic 自动生成icon和splash图片', index: '50462985'},
-  {title: 'Promise执行链', index: '50442417'},
-  {title: 'MongoDB 索引建立机制', index: '47343369'}
+  {title: 'Common Lisp 概览', index: '42645303'},
+  // {title: '两道Javascript-前端面试题', index: '56844013'},
+  // {title: 'Activiti 自定义事件监听和邮件通知', index: '54342060'},
+  // {title: 'Activiti 自定义用户管理和组管理', index: '54315929'},
+  // {title: '如何生成可导入数据库的亿级别数据', index: '52185350'},
+  // {title: '<MongoDB | Mysql>亿级别---数据生成及高效率导入', index: '52181366'},
+  // {title: 'Express session 覆盖或重写', index: '51862761'},
+  // {title: 'Angularjs之ngModel中的值验证绑定', index: '51373126'},
+  //
+  // {title: 'jQuery 技术讨论', index: '50687942'},
+  // {title: 'Ionic 自动生成icon和splash图片', index: '50462985'},
+  // {title: 'Promise执行链', index: '50442417'},
+  // {title: 'MongoDB 索引建立机制', index: '47343369'}
 ];
 
 function getCsdnUrl(index) {
@@ -37,8 +38,10 @@ function visit(site, times) {
       if (times < target) {
         visit(site, times);
       }
+    }).catch((err) => {
+      console.error(err);
     })
-  }, 5 * 1000);
+  }, (sites.length + 1) * 1000);
 }
 
 
